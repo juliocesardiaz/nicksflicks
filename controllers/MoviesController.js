e5e5d28e3b749ebb1c63f864481304b27f1d599a
lujxio <jcdiaz92@gmail.com>
2015-10-02 18:56:54 -0700

    fixes movie displaying bug

--- controllers/MoviesController.js
+++ controllers/MoviesController.js
@@ -1,4 +1,9 @@
 nickflick.controller('MoviesCtrl', function MoviesCtrl($scope, MoviesFactory) {
 	$scope.movies = MoviesFactory.movies;
 	$scope.MoviesFactory = MoviesFactory;
+	$scope.movie = $scope.movies[0];
+	
+	$scope.displayMovie = function(element) {
+		$scope.movie = $scope.movies[element];
+	}
 });
